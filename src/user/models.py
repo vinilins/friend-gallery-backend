4a6b79a5df5cd6c07a989788baca47db7ee33ac8
Vinícius Lins <viniciusaclins@gmail.com>
2022-05-31 20:26:05 -0300

    feat(user/models.py): Change in the field type email
    Changed the field type email
    for EmailField, for validation
    on register

--- src/user/models.py
+++ src/user/models.py
@@ -1,6 +1,7 @@
 from mongoengine import (
     BooleanField,
     Document,
+    EmailField,
     StringField,
 )
 
@@ -9,6 +10,6 @@ class User(Document):
     """User Model of the System"""
 
     name = StringField(required=True, max_length=255)
-    email = StringField(required=True, max_length=255, unique=True)
+    email = EmailField(required=True)
     is_bride_or_groom = BooleanField(default=False, required=True)
     password = StringField(required=True, max_length=255)
